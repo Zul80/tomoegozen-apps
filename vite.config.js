147d812b728b61b98e3ecff04e7d719e3f9af260
Zul80 <zulian.nz80@gmail.com>
2025-11-21 05:44:43 +0700

    upload ulang

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
+            buildDirectory: 'build', // folder build akan berada di public/build
         }),
-        tailwindcss(),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
+
+    // fix untuk hosting (path relative)
+    base: '',
 });
